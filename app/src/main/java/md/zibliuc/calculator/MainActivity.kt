4b6a5555f9aeb837a169e15e7045103111956bc4
Zibliuc Vagid <vagidzibluk@gmail.com>
2025-10-17 20:16:30 +0300

    Add simple listeners for input buttons

--- app/src/main/java/md/zibliuc/calculator/MainActivity.kt
+++ app/src/main/java/md/zibliuc/calculator/MainActivity.kt
@@ -5,16 +5,51 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import md.zibliuc.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding
+        get() = _binding ?: throw IllegalStateException("ActivityMainBinding does not initialized")
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            btnNumPadDot.setOnClickListener { writeToScreen(btnNumPadDot.text.toString()) }
+            btnNumPadZero.setOnClickListener { writeToScreen(btnNumPadZero.text.toString()) }
+            btnNumPadOne.setOnClickListener { writeToScreen(btnNumPadOne.text.toString()) }
+            btnNumPadTwo.setOnClickListener { writeToScreen(btnNumPadTwo.text.toString()) }
+            btnNumPadThree.setOnClickListener { writeToScreen(btnNumPadThree.text.toString()) }
+            btnNumPadFour.setOnClickListener { writeToScreen(btnNumPadFour.text.toString()) }
+            btnNumPadFive.setOnClickListener { writeToScreen(btnNumPadFive.text.toString()) }
+            btnNumPadSix.setOnClickListener { writeToScreen(btnNumPadSix.text.toString()) }
+            btnNumPadSeven.setOnClickListener { writeToScreen(btnNumPadSeven.text.toString()) }
+            btnNumPadEight.setOnClickListener { writeToScreen(btnNumPadEight.text.toString()) }
+            btnNumPadNine.setOnClickListener { writeToScreen(btnNumPadNine.text.toString()) }
+            btnNumPadPlus.setOnClickListener { writeToScreen(btnNumPadPlus.text.toString()) }
+            btnNumPadMinus.setOnClickListener { writeToScreen(btnNumPadMinus.text.toString()) }
+            btnNumPadMultiple.setOnClickListener { writeToScreen(btnNumPadMultiple.text.toString()) }
+            btnNumPadDivide.setOnClickListener { writeToScreen(btnNumPadDivide.text.toString()) }
+
+            btnNumPadDelete.setOnClickListener { clearLastCharacter() }
+            btnNumPadClearAll.setOnClickListener {
+                binding.twCurrentInput.text = ""
+                binding.twPreviousInput.text = ""
+            }
         }
+    }
+
+    fun writeToScreen(item: String) {
+        val currentText = binding.twCurrentInput.text.toString()
+        binding.twCurrentInput.text = currentText.plus(item)
+    }
+
+    fun clearLastCharacter() {
+        val deletedInput: CharSequence = binding.twCurrentInput.text.dropLast(1)
+        binding.twCurrentInput.text = deletedInput
     }
 }
