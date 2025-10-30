3dd55d41bdea180cad675ea137749792f9b26397
Zibliuc Vagid <vagidzibluk@gmail.com>
2025-10-30 22:39:48 +0200

    Add logic for calculationg simple expressions

--- app/src/main/java/md/zibliuc/calculator/core/app/Calculator.kt
+++ app/src/main/java/md/zibliuc/calculator/core/app/Calculator.kt
@@ -3,19 +3,60 @@ package md.zibliuc.calculator.core.app
 import java.util.regex.Pattern
 
 object Calculator {
-    private val symbols: List<Char> = listOf(
-        '+', '-', '/', '*'
+    private val symbols: Map<Int, Char> = mapOf(
+        0 to '+',
+        1 to '-',
+        2 to '*',
+        3 to '/',
     )
+    //need to change priority of operation and output
+    fun calculate(expression: String) : String {
 
-    fun calculate(expression: String) : Double {
+        val expression: MutableList<String> = parse(expression)
 
+        for (i in (symbols.size - 1) downTo 0) {
+            val operation: String? = symbols[i]?.toString()
 
-        return 0.0
+            operation?.let {
+                while (expression.contains(operation)) {
+                    val currentOperation = expression.find({ it == operation })
+
+                    currentOperation?.let {
+                        val indexOfOperation = expression.indexOf(currentOperation)
+                        val result =
+                            calculatePair(expression[indexOfOperation - 1], currentOperation, expression[indexOfOperation + 1])
+                        //debug
+                        println("Pair: " + expression[indexOfOperation - 1] + currentOperation
+                                + expression[indexOfOperation + 1] + "=" + result)
+
+                        expression[indexOfOperation] = result
+                        expression.removeAt(indexOfOperation + 1)
+                        expression.removeAt(indexOfOperation - 1)
+                    }
+                }
+            }
+        }
+
+        return expression[0]
     }
 
-    fun parse(input: String) : List<String> {
+    fun calculatePair(x: String, action: String, y: String) : String {
+        val xValue = x.toDouble()
+        val yValue = y.toDouble()
+
+        val result = when (action) {
+            "*" -> xValue * yValue
+            "/" -> xValue / yValue
+            "-" -> xValue - yValue
+            "+" -> xValue + yValue
+            else -> 0
+        }
+        return result.toString()
+    }
+
+    fun parse(input: String) : MutableList<String> {
         val numbers = input.split("+", "-", "/", "*")
-        val operations = input.filter { it in symbols }
+        val operations = input.filter { it in symbols.values }
 
         val expression: MutableList<String> = mutableListOf()
 
--- app/src/test/java/md/zibliuc/calculator/core/app/CalculatorTest.kt
+++ app/src/test/java/md/zibliuc/calculator/core/app/CalculatorTest.kt
@@ -6,9 +6,52 @@ import org.junit.Assert.*
 
 class CalculatorTest {
     @Test
-    fun calculate() {
-        // TODO
+    fun calculate_SumTwoNumbers() {
+        val expression = "9+6"
+        val expectation = "15.0"
+        val actual = Calculator.calculate(expression)
+
+        assertEquals(expectation, actual)
     }
+
+    @Test
+    fun calculate_SumThreeNumbers() {
+        val expression = "4+5+6"
+        val expectation = "15.0"
+        val actual = Calculator.calculate(expression)
+
+        assertEquals(expectation, actual)
+    }
+
+    @Test
+    fun calculate_MultiplyAndSumThreeNumbers() {
+        val expression = "4+5*6"
+        val expectation = "34.0"
+        val actual = Calculator.calculate(expression)
+
+        assertEquals(expectation, actual)
+    }
+
+    @Test
+    fun calculate_BigExpression() {
+        //65+32-68/21+44*22/74*0.17
+        val expression = "65+32-68/21+44*22/74*0.17"
+        val expectation = "95.9856885457"
+        val actual = Calculator.calculate(expression)
+
+        assertEquals(expectation, actual)
+    }
+    /*
+    @Test
+    fun calculate_MultiplyAndMinusThreeNumbers() {
+        //bagreport suka (need to fix parsing with minus in front)
+        val expression = "-4-5*6"
+        val expectation = "-26.0"
+        val actual = Calculator.calculate(expression)
+
+        assertEquals(expectation, actual)
+    }
+    */
 
     @Test
     fun parse_ShouldReturnTwoArray() {
@@ -21,4 +64,40 @@ class CalculatorTest {
         assertEquals(expectation, actual)
     }
 
+    @Test
+    fun calculatePair_ShouldMultiplyTwoIntegerNumbers() {
+        val expectation = "33.0"
+
+        val actual = Calculator.calculatePair("11", "*", "3")
+
+        assertEquals(expectation, actual)
+    }
+
+
+    @Test
+    fun calculatePair_ShouldDivideTwoIntegerNumbers() {
+        val expectation = "11.0"
+
+        val actual = Calculator.calculatePair("33", "/", "3")
+
+        assertEquals(expectation, actual)
+    }
+
+    @Test
+    fun calculatePair_ShouldDivideTwoIntegerNumbersWithDoubleResult() {
+        val expectation = "0.25"
+
+        val actual = Calculator.calculatePair("1", "/", "4")
+
+        assertEquals(expectation, actual)
+    }
+
+    @Test
+    fun calculatePair_ShouldDivideTwoDoubleNumbersWithDoubleResult() {
+        val expectation = "0.5"
+
+        val actual = Calculator.calculatePair("0.25", "/", "0.5")
+
+        assertEquals(expectation, actual)
+    }
 }
